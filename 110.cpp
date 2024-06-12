3721c1d9e0e3c588a69ba01b536340cc726228bc
maryam abbasi <169179074+mary6776abbsi@users.noreply.github.com>
2024-06-12 19:19:01 +0330

    110

---
+++ 110.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <regex>
+using namespace std;
+
+// تابع برای اعتبارسنجی ایمیل
+bool validateEmail(string email) {
+    // الگوی ایمیل به کمک عبارت منظم
+    regex pattern("(\\w+)(\\.|_)?(\\w*)@(\\w+)(\\.(\\w+))+");
+
+    // بررسی مطابقت ایمیل با الگو
+    return regex_match(email, pattern);
+}
+
+int main() {
+    string email;
+
+    // دریافت ایمیل از کاربر
+    cout << "Enter your email address: ";
+    cin >> email;
+
+    // اعتبارسنجی ایمیل و چاپ نتیجه
+    if (validateEmail(email)) {
+        cout << "Valid email address!" << endl;
+    }
+    else {
+        cout << "Invalid email address!" << endl;
+    }
+
+    return 0;
+}
